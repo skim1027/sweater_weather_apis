517c5d24b8fc47a4a9ac471540ce1de8f00aed8e
Sooyung Kim <kimx1279@gmail.com>
2024-01-12 18:50:53 -0700

    Add keys

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -2,6 +2,8 @@ class Api::V0::ForecastController < ApplicationController
   def index
     location = params[:location]
 
+    lat_lon = LocationFacade.new.city_state(location)
+
     
   end
 end
---
+++ app/facades/location_facade.rb
@@ -0,0 +1,5 @@
+class LocationFacade 
+  def city_state(location)
+    data = LocationService.new
+  end
+end

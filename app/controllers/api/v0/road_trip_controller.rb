88f76b5567db2c80c575b1984550b32d8de2ccc7
Sooyung Kim <kimx1279@gmail.com>
2024-01-15 18:23:36 -0700

    Update road trip controller

--- app/controllers/api/v0/road_trip_controller.rb
+++ app/controllers/api/v0/road_trip_controller.rb
@@ -4,12 +4,12 @@ class Api::V0::RoadTripController < ApplicationController
     from = params[:origin]
     to = params[:destination]
     if user
-      lat_lon = LocationFacade.new.city_state(to).lat_lon
-      total_travel_time = LocationFacade.new.directions(from, to).travel_time
-      hours = LocationFacade.new.directions(from, to).time_in_hrs
-      eta_time = (DateTime.now + hours.hours).strftime("%Y-%m-%d %H")
-      weather = WeatherFacade.new.location_weather(lat_lon).weather_at_eta(eta_time)
-      render json: RoadTripSerializer.data(weather, total_travel_time, from, to), status: :ok
+      # lat_lon = LocationFacade.new.city_state(to).lat_lon
+      # total_travel_time = LocationFacade.new.directions(from, to).travel_time
+      # hours = LocationFacade.new.directions(from, to).time_in_hrs
+      # eta_time = (DateTime.now + hours.hours).strftime("%Y-%m-%d %H")
+      # weather = WeatherFacade.new.location_weather(lat_lon).weather_at_eta(eta_time)
+      render json: RoadTripSerializer.data(weather(lat_lon(to), hours(from, to)), total_travel_time(from, to), from, to), status: :ok
     else
       render json: { errors: [title: "Please provide correct API key", status: "400"] }, status: :bad_request
     end
@@ -17,25 +17,25 @@ class Api::V0::RoadTripController < ApplicationController
 
   # if user is not there, return error, direction !=, then error, 
 
-  # private
+  private
 
-  # def lat_lon(location)
-  #   LocationFacade.new.city_state(location).lat_lon
-  # end
+  def lat_lon(location)
+    LocationFacade.new.city_state(location).lat_lon
+  end
 
-  # def total_travel_time(from, to)
-  #   LocationFacade.new.directions(from, to).travel_time
-  # end
+  def total_travel_time(from, to)
+    LocationFacade.new.directions(from, to).travel_time
+  end
 
-  # def hours(from, to)
-  #   LocationFacade.new.directions(from, to).time_in_hrs
-  # end
+  def hours(from, to)
+    LocationFacade.new.directions(from, to).time_in_hrs
+  end
 
-  # def weather(lat_lon)
-  #   WeatherFacade.new.location_weather(lat_lon).weather_at_eta
-  # end
+  def weather(lat_lon, hrs)
+    WeatherFacade.new.location_weather(lat_lon).weather_at_eta(eta_time(hrs))
+  end
   
-  # def eta_time(hours)
-  #   (DateTime.now + hours.hours).strftime("%Y-%m-%d %H")
-  # end
+  def eta_time(hours)
+    (DateTime.now + hours.hours).strftime("%Y-%m-%d %H")
+  end
 end
